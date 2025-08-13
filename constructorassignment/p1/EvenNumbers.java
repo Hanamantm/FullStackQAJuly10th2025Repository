cdf94170ef8f31791035d986b2dc3527c800a286
Hanamantm <hanamantm1990@gmail.com>
2025-08-13 22:26:32 +0530

    This is my second program saving to github

---
+++ constructorassignment/p1/EvenNumbers.java
@@ -0,0 +1,15 @@
+package com.sgtesting.constructorassignment.p1;
+
+public class EvenNumbers
+{
+    public static void main(String[] args) {
+
+        for (int i =0; i<=10; i++)
+        {
+            if(i%2 ==0)
+            {
+                System.out.println("Even numbers :" +i);
+            }
+        }
+    }
+}
